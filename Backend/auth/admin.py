2976b7b97c831b4f9472766c7673b10b76a16b23
Samuel <116428301+Sam-3l@users.noreply.github.com>
2025-03-19 13:48:45 +0100

    feat: initialize auth, add auth user model and admin

---
+++ Backend/auth/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+class CustomUserAdmin(UserAdmin):
+    """ Custom admin panel to manage users using email. """
+    model = User
+    list_display = ("email", "first_name", "last_name", "is_staff", "is_active")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "is_staff", "is_active"),
+        }),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
