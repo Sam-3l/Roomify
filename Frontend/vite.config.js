b8b4cf050d7b9798b26ee690c7abee408cef1d65
noiz-x <oloyedeelijah23@gmail.com>
2025-03-20 19:02:11 +0100

    fix: update Vite configuration for Tailwind CSS and server allowed hosts

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(
-    
-  )],
+  plugins: [react(), tailwindcss()],
   server:{
-    allowedHosts:["2f1b864061a2a5deb5eeb31ce6992b41.serveo.net"]
+    allowedHosts:["all"]
   }
 })
