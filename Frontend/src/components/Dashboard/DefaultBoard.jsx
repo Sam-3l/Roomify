19f459a995f5c4681ce3a38a0f0acadf70ffc6c5
noiz-x <oloyedeelijah23@gmail.com>
2025-06-29 14:33:07 +0100

    refactor: update DefaultBoard layout to improve structure and scrolling behavior

--- Frontend/src/components/Dashboard/DefaultBoard.jsx
+++ Frontend/src/components/Dashboard/DefaultBoard.jsx
@@ -2,15 +2,13 @@ import PseudoProfile from "./PseudoProfile"
 import PseudoTimetable from "./PseudoTimetable"
 import PseudoNotification from "./PseudoNotification"
 import PseudoAvailableHalls from "./PseudoAvailableHalls"
-import ClassSummary from "./ClassSummary"
 export default function DefaultBoard() {
     return(
-        <aside className="col-span-1 bg-bg md:pr-4 md:col-span-6 lg:col-span-7 overflow-y-auto flex flex-col gap-4 rounded-xl">
+        <div className="col-span-1 bg-bg md:pr-4 md:col-span-7 overflow-auto md:overflow-y-scroll  flex flex-col gap-4 rounded-xl">
             <PseudoProfile/>
             <PseudoTimetable/>
             <PseudoNotification/>
             <PseudoAvailableHalls/>
-            <ClassSummary/>
-        </aside>
+        </div>
     )
 }
